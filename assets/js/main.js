ca3b695732d64a062e1cd8c55b62bc2b3fa43c41
Ella Choy <egal21de@gmail.com>
2021-09-04 20:38:00 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,47 @@
 console.log('hi');
-const size = Number(document.getElementById("size").value)
-const age = Number(document.getElementById("age").value)
+const size =document.getElementById("size");
+const age = document.getElementById("age");
 const breite = document.getElementById("breite")
-const schmale = document.getElementById("schmale")
-let isbreite=true
+// const schmale = document.getElementById("schmale")
+// let isbreite=true
 
     function rechnen(){
         // console.log("rechnen");
-        if (isbreite){
-            document.getElementById("result").innerHTML=`Dein Idealgewicht ist:${schmaleKoerper}`;
+       let num_Size=Number(size.value);// werte von size
+       let num_Age=Number(age.value);// werte von age
+
+
+
+        if (breite.checked){
+            document.getElementById("result").innerHTML=`Dein Idealgewicht ist:${breiteKoerper(num_Size, num_Age)}`;
+           
 
         }else{
-            document.getElementById("result").innerHTML=`Dein Idealgewicht ist:${breiteKoerper}`;
+            document.getElementById("result").innerHTML=`Dein Idealgewicht ist:${schmaleKoerper(num_Size, num_Age)}`;
         }
     }
 
-   function schmaleKoerper(size, age){
+   function schmaleKoerper(size,age){
        return (size - 100) + (age / 10) * 0.9 * 0.9;
    }
-   schmaleKoerper();
+//    schmaleKoerper();
    function breiteKoerper(size, age){
     return (size - 100) + (age / 10) * 0.9 * 1.1;
    }
-   breiteKoerper();
-   function getSize(){
-       if(breite.checked){
-           console.log("breite");
-       }else{
-           console.log('schmale');
-       }
-   }
+
+//    rausnehmen
+//    breiteKoerper();
+//    function getSize(){
+//        if(breite.checked){
+//            console.log("breite");
+//        }else{
+//            console.log('schmale');
+//        }
+//    }
  
+
+//  ersetzen !!!
+// if(breite.checked) {
+//     let gewicht = breiteKoerper(num_Size, num_Age);
+//     document.getElemnentByID(...).innerHtml = '...' + gewicht
+//   }
